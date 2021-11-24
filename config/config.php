4de7224aa51627cae46354af7b7f6a4766604b99
GuillaumeAssailly <guillaume.assailly@gmail.com>
2021-11-24 16:34:31 +0100

    ajout d'un dossier de config et un script contenant l'emplacement des vues, les login

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$rep=__DIR__.'/../';
+
+//Base de Données
+$base="dbguassailly";
+$login="root";
+$mdp="";
+
+//Liste dse vues
+$vues['vue1']='vues/vue1.php';
+$vues['admin']='vues/admin.php';
+$vues['erreur']='vues/erreur.php';
+$vues['login']='vues/login.php';
